39484ea6a80396864654344ce0b402673b2e1e35
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-03 20:21:35 +0500

    Fix: Naming

--- Algorithms/Sort/bubbleSort.cpp
+++ Algorithms/Sort/bubbleSort.cpp
@@ -4,16 +4,12 @@ using namespace std;
 class Solution
 {
 public:
-    void bubbleSort(vector<int>& nums)
-    {
+    void bubbleSort(vector<int>& nums) {
         bool isSwapped;
-        for(int i=0;i<nums.size();i++)
-        {
+        for(int i=0;i<nums.size();i++) {
             isSwapped=false;
-            for(int j=0;j<nums.size()-1-i;j++)
-            {
-                if(nums[j]>nums[j+1])
-                {
+            for(int j=0;j<nums.size()-1-i;j++) {
+                if(nums[j]>nums[j+1]) {
                     swap(nums[j],nums[j+1]);
                     isSwapped=true;
                 }
@@ -22,12 +18,10 @@ public:
         }
     }
 };
-int main()
-{
+int main() {
     Solution s;
     vector<int> nums[3]={{2,0,4,5,1,3},{2,1,2},{13,4,51,2,6,73,7,8}};
-    for(int i=0;i<3;i++)
-    {
+    for(int i=0;i<3;i++) {
         s.bubbleSort(nums[i]);
         for(int j:nums[i]) cout<<j<<" ";
         cout<<'\n';
