3ea06891908423b72e7cee2df188400dd208c290
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-04 22:59:50 +0500

    Add: Renaming

---
+++ LeetCode/2.Medium/109.cpp
@@ -0,0 +1,40 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/*
+Approach: Recursion / Divide And Conquer / Slow & Fast Pointer
+TC: O(nlog n)
+SC: O(log n)
+*/
+
+class Solution {
+    TreeNode* makeBST(ListNode* head,ListNode* end) {
+        if(head==end) return nullptr;
+        ListNode *slow=head,*fast=head;
+        while(fast!=end && fast->next!=end) {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        TreeNode* node=new TreeNode(slow->val);
+        node->left=makeBST(head,slow);
+        node->right=makeBST(slow->next,end);
+        return node;
+    }
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        return makeBST(head,nullptr);
+    }
+};
