230c72e52b6ca163ba28f1961bfe00033631839a
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-06 15:41:30 +0500

    Update: Concise

--- LeetCode/1.Easy/1.cpp
+++ LeetCode/1.Easy/1.cpp
@@ -12,19 +12,13 @@ SC: O(n)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums,int target) {
-        vector<int> r;
         unordered_map<int,int> m;
-        unordered_map<int,int>::iterator it;
         for(int i=0;i<nums.size();i++) {
-            it=m.find(target-nums[i]);
-            if(it!=m.end()) {
-                r.push_back(it->second);
-                r.push_back(i);
-                break;
-            }
+            auto it=m.find(target-nums[i]);
+            if(it!=m.end()) return {it->second,i};
             m[nums[i]]=i;
         }
-        return r;
+        return {};
     }
 };
 int main() {
