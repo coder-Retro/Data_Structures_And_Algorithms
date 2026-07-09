72cc3bc934eee55f0c4138759f5e9c83c92e6f7c
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-10 01:21:10 +0500

    Update: Vector Implementation

--- Data_Structures/1.Vector/main.cpp
+++ Data_Structures/1.Vector/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorithm>
 #include"vector"
 using namespace std;
 // Main Function
@@ -9,11 +10,11 @@ int main()
     cout<<"Enter size of vector : ";
     cin>>sizeOfVec;
     cout<<"Enter Vector : ";
-    for(int i=0,val;i<sizeOfVec;i++)
-    {
+    for(int i=0,val;i<sizeOfVec;i++) {
         cin>>val;
         vec.push_back(val);
     }
+    sort(vec.begin(),vec.end());
     cout<<"Vector : ";
     for(int i:vec)
         cout<<i<<" ";
